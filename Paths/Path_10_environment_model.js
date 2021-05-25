490dc2ef91480614b05e681baf2efd4bbab9a45b
Chen Hsiao Ting <68311371+hsiaotingluv@users.noreply.github.com>
2021-05-25 14:03:00 +0800

    Create Path_10_environment_model.js

---
+++ Paths/Path_10_environment_model.js
@@ -0,0 +1,47 @@
+// Question 1
+Assignments to the name my_p are possible, using my_p = pair(3, 4);
+
+// Question 2
+An environment is a sequence of frames.
+
+// Question 3
+A frame is a set of name bindings.
+
+// Question 4
+The interpreter looks up the variable in the frame that is enclosing the current frame.
+
+// Question 5
+The interpreter issues an error message “Name not declared”.
+
+// Question 6
+The interpreter uses the same environment in which the function application is evaluated.
+
+// Question 7
+The interpreter uses a new environment, which results from extending the environment stored in the function object that the name f refers to.
+
+// Question 8
+Only the parameters of the function being called.
+
+// Question 9
+In the current frame if it has a binding of x or the closest surrounding frame that has a binding of x.
+
+// Question 10
+The environment in which the statement gets evaluated.
+
+// Question 11
+2
+
+// Question 12
+10
+
+// Question 13
+9
+
+// Question 14
+4
+
+// Question 15
+10
+
+// Question 16
+3

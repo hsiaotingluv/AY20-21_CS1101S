be275785dd31d225e5aff99c1f4fb63dd9341e50
Chen Hsiao Ting <68311371+hsiaotingluv@users.noreply.github.com>
2021-05-25 14:01:49 +0800

    Create Path_4_orders_of_growth.js

---
+++ Paths/Path_4_orders_of_growth.js
@@ -0,0 +1,23 @@
+// Question 1
+The function t has order of growth of Θ(n)
+
+// Question 2
+The function s has order of growth of Θ(n)
+
+// Question 3
+The function t has order of growth of Θ(n)
+
+// Question 4
+The function s has order of growth of Θ(1)
+
+// Question 5
+The function t has order of growth of Θ(2^n)
+
+// Question 6
+a*n
+
+// Question 7
+The function t has order of growth of Θ(n)
+
+// Question 8
+The function t has order of growth of Θ(log(n))

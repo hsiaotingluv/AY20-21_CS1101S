95cf3cbd6e2b8f1b90862f26e5c33137054b4c14
Chen Hsiao Ting <68311371+hsiaotingluv@users.noreply.github.com>
2021-05-25 14:01:20 +0800

    Create Path_2_runes.js

---
+++ Paths/Path_2_runes.js
@@ -0,0 +1,18 @@
+// Question 1
+No
+
+// Question 2
+No
+
+// Question 3
+No
+
+// Question 4
+function love(rune) {
+    return stack(red(heart), rune);
+}
+
+show(love(blue(nova)));
+
+// Question 5
+4

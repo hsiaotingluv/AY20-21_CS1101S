6765cb19e232ae756edce3b291bff94464faa9e3
Chen Hsiao Ting <68311371+hsiaotingluv@users.noreply.github.com>
2021-05-25 14:02:47 +0800

    Create Path_9_mutable_data.js

---
+++ Paths/Path_9_mutable_data.js
@@ -0,0 +1,27 @@
+// Question 1
+Error, because it is trying to assign value to the constant x in the statement x=1;
+
+// Question 2
+2
+
+// Question 3
+2
+
+// Question 4
+3
+
+// Question 5
+15
+
+// Question 6
+Error, because it is trying to assign a value to the constant p in the statement p = pair(3, 4);
+
+// Question 7
+[3, [1, 2]]
+
+// Question 8
+[[1, 2], 4]
+
+// Question 9
+A cyclic list of elements 1, 2, 3
+

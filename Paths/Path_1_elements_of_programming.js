e8b5cbaf793624aac4131bc3b70b9ed7a0338a20
Chen Hsiao Ting <68311371+hsiaotingluv@users.noreply.github.com>
2021-05-25 14:01:07 +0800

    Create Path_1_elements_of_programming.js

---
+++ Paths/Path_1_elements_of_programming.js
@@ -0,0 +1,24 @@
+// Question 1
+2+3
+
+// Question 2
+25
+
+// Question 3
+const foo = 10;
+const bar = 10 + 20;     
+const foobar = foo + bar;
+
+// Question 4
+function bar() {
+    return 20;
+}
+
+// Question 5
+function square(x) {
+    return x * x;      
+}
+
+function square_of_difference(x, y) {
+    return x >= y ? square(x - y) : square(y - x);
+}
